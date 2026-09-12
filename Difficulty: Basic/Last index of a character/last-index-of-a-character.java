b2f03955cd5e4dcf0714221620ce4c885e084dc9
Venky1234923 <gogamvenkatalakshmi8@gmail.com>
2026-09-12 21:49:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last index of a character/last-index-of-a-character.java
@@ -0,0 +1,11 @@
+class Solution {
+    int LastIndex(String s, char c) {
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) == c) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
