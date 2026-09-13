a0d0c51814980a9775df79ed7901ce769af9d5be
Venky1234923 <gogamvenkatalakshmi8@gmail.com>
2026-09-13 19:48:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Duplicates Removal/string-duplicates-removal.java
@@ -0,0 +1,17 @@
+class Solution {
+    String removeDuplicates(String s) {
+        boolean[] seen = new boolean[256];
+        String result = "";
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(!seen[c]) {
+                result = result + c;
+                seen[c] = true;
+            }
+        }
+
+        return result;
+    }
+}
